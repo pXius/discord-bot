cf127007103e24237719b3ddc2ffc38aae360fd8
Hermanus Engelbrecht <fan.engelbrecht@gmail.com>
2022-02-09 15:58:57 +0100

    Cleanup and Creating Discord integration base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IntegrationDiscordModule } from './integration-discord/integration-discord.module';
 
 @Module({
-  imports: [],
+  imports: [IntegrationDiscordModule],
   controllers: [AppController],
   providers: [AppService],
 })
